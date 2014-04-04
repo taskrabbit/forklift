29882136734a6c68931ebb3bb70e983faf6135f1
Evan Tahler <evan@taskrabbit.com>
2014-04-04 14:24:48 -0700

    more mysql unit tests

--- spec/integration/mysql_spec.rb
+++ spec/integration/mysql_spec.rb
@@ -1,17 +1,72 @@
 require 'spec_helper'
 
 describe 'mysql' do  
-  it "can read data (raw)"
-  it "can read data (filtered)"
 
-  it "can write new data"
-  it "can update existing data"
+  before(:each) do
+    SpecSeeds.setup_mysql
+  end
 
-  it "can delte a table"
-  it "can drop a database"
-  it "can read a list of tables"
-  it "can truncate a table"
-  it "can get the cols of a table"
+  it "can read data (raw)" do
+    query = 'select * from `users`'
+    plan = SpecPlan.new
+    @rows = []
+    plan.do! {
+      source = plan.connections[:mysql][:forklift_test_source_a]
+      source.read(query) {|data| 
+        @rows = (@rows + data)
+      }
+    }
+
+    expect(@rows.length).to eql 5
+  end
+  
+  it "can read data (filtered)" do
+    query = 'select * from `users`'
+    plan = SpecPlan.new
+    @rows = []
+    plan.do! {
+      source = plan.connections[:mysql][:forklift_test_source_a]
+      source.read(query, source.current_database, false, 3, 0) {|data| 
+        @rows = (@rows + data)
+      }
+    }
+
+    expect(@rows.length).to eql 3
+  end
+
+  it "can write new data" do
+    table = "users"
+    data = [
+      {:email => 'other@example.com', :first_name => 'other', :last_name => 'n', :created_at => Time.new.to_s(:sql), :updated_at => Time.new.to_s(:sql)}
+    ]
+    plan = SpecPlan.new
+    plan.do! {
+      destination = plan.connections[:mysql][:forklift_test_source_a]
+      destination.write(data, table)
+    }
+
+    destination = SpecClient.mysql('forklift_test_source_a')
+    count = destination.query('select count(1) as "count" from users').first['count']
+    expect(count).to eql 6
+  end
+
+  it "can update existing data" do 
+    table = "users"
+    data = [
+      {:id => 1, :email => 'evan@example.com', :first_name => 'New Name', :last_name => 'T', :created_at => Time.new.to_s(:sql), :updated_at => Time.new.to_s(:sql)}
+    ]
+    plan = SpecPlan.new
+    plan.do! {
+      destination = plan.connections[:mysql][:forklift_test_source_a]
+      destination.write(data, table)
+    }
+
+    destination = SpecClient.mysql('forklift_test_source_a')
+    count = destination.query('select count(1) as "count" from users').first['count']
+    expect(count).to eql 5
+    first_name = destination.query('select first_name from users where id = 1').first['first_name']
+    expect(first_name).to eql 'New Name'
+  end
   
   it "can lazy-create a table with primary keys provided"
   it "can lazy-create a table without primary keys provided"
--- spec/unit/connection/mysql_spec.rb
+++ spec/unit/connection/mysql_spec.rb
@@ -1,6 +1,78 @@
 require 'spec_helper'
 
 describe Forklift::Connection::Mysql do
+
+  describe "read/write utils" do
+    before(:each) do
+      SpecSeeds.setup_mysql
+    end
+
+    it "can read a list of tables" do
+      plan = SpecPlan.new
+      plan.do! {
+        source = plan.connections[:mysql][:forklift_test_source_a]
+        expect(source.tables).to include 'users'
+        expect(source.tables).to include 'products'
+        expect(source.tables).to include 'sales'
+      }
+    end
+
+    it "can delte a table" do 
+      plan = SpecPlan.new
+      table = "users"
+      plan.do! {
+        source = plan.connections[:mysql][:forklift_test_source_a]
+        expect(source.tables).to include 'users'
+        source.drop! table
+        expect(source.tables).to_not include 'users'
+      }
+    end
+
+    it "can count the rows in a table" do
+      plan = SpecPlan.new
+      table = "users"
+      plan.do! {
+        source = plan.connections[:mysql][:forklift_test_source_a]
+        expect(source.count(table)).to eql 5
+      }
+    end
+
+    it "can truncate a table (both with and without !)" do 
+      plan = SpecPlan.new
+      table = "users"
+      plan.do! {
+        source = plan.connections[:mysql][:forklift_test_source_a]
+        expect(source.count(table)).to eql 5
+        source.truncate! table
+        expect(source.count(table)).to eql 0
+        expect { source.truncate(table) }.to_not raise_error
+      }
+    end
+
+    it 'trunacte! will raise if the table does not exist' do
+      plan = SpecPlan.new
+      table = "other_table"
+      plan.do! {
+        source = plan.connections[:mysql][:forklift_test_source_a]
+        expect { source.truncate!(table) }.to raise_error(/Table 'forklift_test_source_a.other_table' doesn't exist/)
+      }
+    end
+
+    it "can get the columns of a table" do 
+      plan = SpecPlan.new
+      table = "sales"
+      plan.do! {
+        source = plan.connections[:mysql][:forklift_test_source_a]
+        # ap source.columns(table)
+        expect(source.columns(table)).to include 'id' 
+        expect(source.columns(table)).to include 'user_id' 
+        expect(source.columns(table)).to include 'product_id' 
+        expect(source.columns(table)).to include 'timestamp' 
+      }
+    end
+
+  end
+
   describe "#safe_values" do
     subject { described_class.new({}, {}) }
 
