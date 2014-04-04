ba5c26a1f8c2d31bd6d3bb0893b207bf0ef2f566
Evan Tahler <evan@taskrabbit.com>
2014-04-04 16:23:00 -0700

    lazy table create

--- spec/integration/mysql_spec.rb
+++ spec/integration/mysql_spec.rb
@@ -67,8 +67,72 @@ describe 'mysql' do
     first_name = destination.query('select first_name from users where id = 1').first['first_name']
     expect(first_name).to eql 'New Name'
   end
-  
-  it "can lazy-create a table with primary keys provided"
-  it "can lazy-create a table without primary keys provided"
-  
+
+  describe 'lazy create' do
+
+    after(:each) do
+      destination = SpecClient.mysql('forklift_test_source_a')
+      destination.query('drop table if exists `new_table`')
+    end
+    
+    it "can lazy-create a table with primary keys provided" do 
+      data = [
+        {:id => 1, :thing => 'stuff a', :updated_at => Time.new},
+        {:id => 2, :thing => 'stuff b', :updated_at => Time.new},
+        {:id => 3, :thing => 'stuff c', :updated_at => Time.new},
+      ]
+      table = "new_table"
+      plan = SpecPlan.new
+      plan.do! {
+        destination = plan.connections[:mysql][:forklift_test_source_a]
+        destination.write(data, table)
+      }
+
+      destination = SpecClient.mysql('forklift_test_source_a')
+      cols = []
+      destination.query("describe #{table}").each do |row|
+        cols << row["Field"]
+        case row["Field"]
+        when "id" 
+          expect(row["Type"]).to eql "int(11)"
+        when "thing" 
+          expect(row["Type"]).to eql "text"
+        when "updated_at" 
+          expect(row["Type"]).to eql "datetime"
+        end
+      end
+      expect(cols).to eql ['id', 'thing', 'updated_at']
+    end
+    
+    it "can lazy-create a table without primary keys provided" do
+      data = [
+        {:thing => 'stuff a', :updated_at => Time.new},
+        {:thing => 'stuff b', :updated_at => Time.new},
+        {:thing => 'stuff c', :updated_at => Time.new},
+      ]
+      table = "new_table"
+      plan = SpecPlan.new
+      plan.do! {
+        destination = plan.connections[:mysql][:forklift_test_source_a]
+        destination.write(data, table)
+      }
+
+      destination = SpecClient.mysql('forklift_test_source_a')
+      cols = []
+      destination.query("describe #{table}").each do |row|
+        cols << row["Field"]
+        case row["Field"]
+        when "id" 
+          expect(row["Type"]).to eql "int(11)"
+        when "thing" 
+          expect(row["Type"]).to eql "text"
+        when "updated_at" 
+          expect(row["Type"]).to eql "datetime"
+        end
+      end
+      expect(cols).to eql ['id', 'thing', 'updated_at']
+    end
+
+  end
+
 end
